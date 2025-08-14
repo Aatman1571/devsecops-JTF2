3d175f86ae873aa88aa945209f87fd0e12fa6003
Aatman1571 <aatu15701@gmail.com>
2025-08-14 11:59:21 -0400

    Initial commit with CI pipeline

--- iac/terraform/main.tf
+++ iac/terraform/main.tf
@@ -1,6 +1,11 @@
 provider "local" {}
 
-resource "local_file" "simulate" {
-  content  = "Provisioned by Terraform!"
+resource "local_file" "infra" {
+  content  = "Infrastructure provisioned securely"
   filename = "${path.module}/infra.txt"
+}
+
+resource "local_file" "secure_config" {
+  content  = "Simulated secure settings like IAM and Security Groups"
+  filename = "${path.module}/secure_config.txt"
 }
